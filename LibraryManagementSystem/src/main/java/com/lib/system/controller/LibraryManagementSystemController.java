5d895dc27575f9bb7b0529121e12090e2b482b1d
Nandar <nandarwinsoedev97@gmail.com>
2023-08-29 14:32:34 +0630

    pdf

--- LibraryManagementSystem/src/main/java/com/lib/system/controller/LibraryManagementSystemController.java
+++ LibraryManagementSystem/src/main/java/com/lib/system/controller/LibraryManagementSystemController.java
@@ -1,17 +1,27 @@
 package com.lib.system.controller;
-
+ 
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption; 
 import java.util.HashMap;
 import java.util.Map;
+ 
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.lib.system.entity.Book;
 import com.lib.system.entity.Category;
@@ -80,16 +90,53 @@ public class LibraryManagementSystemController {
 		}
 
 	}
-
 	@PostMapping("/addBookData")
-	public String addConfirm(Model model, @ModelAttribute("form") Book book, @RequestParam("userId") int userId) {
-		book.setUserId(0);
-		this.bookService.addData(book);
-		model.addAttribute("userId", userId);
-		model.addAttribute("form", new Book());
-		model.addAttribute("bookList", this.bookService.getAllBook());
-		model.addAttribute("categoryList", categoryService.getAllCategory());
-		return "index";
+	public String addConfirm(Model model,@RequestParam("file") MultipartFile file, RedirectAttributes ra, @ModelAttribute("form") Book book, @RequestParam("userId") int userId) throws IOException {
+//		book.setUserId(0);
+//		this.bookService.addData(book);
+//		model.addAttribute("userId", userId);
+//		model.addAttribute("form", new Book());
+//		model.addAttribute("bookList", this.bookService.getAllBook());
+//		model.addAttribute("categoryList", categoryService.getAllCategory());
+	//	return "index";
+		
+				// Handle the uploaded file (e.g., save it to a temporary location)
+				// You can save it to a file system, database, or process it as needed.
+
+				// Generate a PDF as an example
+				book.setUserId(0);
+				
+				model.addAttribute("userId", userId);
+				model.addAttribute("form", new Book());
+				model.addAttribute("bookList", this.bookService.getAllBook());
+				model.addAttribute("categoryList", categoryService.getAllCategory());
+				String fileName = StringUtils.cleanPath(file.getOriginalFilename());
+
+				book.setFile(fileName);
+				book.setSize(file.getSize());
+				this.bookService.addData(book);
+				//Document doc = documentRepository.save(book);
+				String uploadDir = "./brand-logos/" + book.getId();
+				Path uploadPath = Paths.get(uploadDir);
+				
+				if(!Files.exists(uploadPath)) {
+					Files.createDirectories(uploadPath);
+				}
+				
+				try (InputStream inputStream = file.getInputStream()){
+					Path filePath = uploadPath.resolve(fileName);
+					System.out.println(filePath.toFile().getAbsolutePath());
+					Files.copy(inputStream, filePath,StandardCopyOption.REPLACE_EXISTING);
+				}catch(IOException e) {
+					throw new IOException("Could not save uploaded File: "+fileName);
+				}
+				
+				this.bookService.addData(book);
+				ra.addFlashAttribute("message", "The file has been uploaded successfully");
+				//return "redirect:/";
+
+				return "index"; // Return to the upload form page
+		
 	}
 
 	@GetMapping("/getAllBook")
